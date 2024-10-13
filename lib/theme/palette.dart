67bb1506a4026605f7f4f08c5e1fe1ad13a56923
Jasveer Singh Kalsi <kalsijasveer2003@gmail.com>
2024-10-13 17:48:53 +0530

    Adding color palette

---
+++ lib/theme/palette.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color scaffoldBackgroundColor = Color.fromRGBO(12, 12, 12, 1.0);
+  static const Color appBarColor = Color.fromRGBO(18, 18, 18, 1.0);
+  static const Color enabledBorderColor = Color.fromRGBO(48, 48, 48, 1);
+  static const Color focusedborderColor = Color.fromRGBO(229, 43, 100, 1.0);
+  static const Color gradient1 = Color.fromRGBO(98, 39, 116, 1);
+  static const Color gradient2 = Color.fromRGBO(197, 51, 100, 1);
+  static const Color gradient3 = Color.fromRGBO(243, 129, 129, 1);
+  static const Color gradient4 = Color.fromRGBO(32, 32, 32, 1);
+  static const Color gradient5 = Color.fromRGBO(64, 64, 64, 1);
+  static const Color gradient6 = Color.fromRGBO(128, 128, 128, 1);
+}
